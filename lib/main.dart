b1ceb26743d97ee729801454a44a8869f0d988c9
Santiago Urbano <david.urbano@uao.edu.co>
2023-07-04 23:15:27 -0500

    fix(main.dart): fix typo in constructor parameter super.key feat(main.dart): add comments to describe the purpose of the build method and the usage of the Provider design pattern feat(main.dart): create an instance of MyAppState using the Provider pattern to manage the application state
    refactor(main.dart): change variable name `useMaterial3` to `useMaterial2`
    feat(main.dart): add support for `history` list to keep track of shown words
    feat(main.dart): add support for `historyListKey` to access animated list state
    feat(main.dart): add support for marking words as favorites and removing them
    feat(main.dart): add support for `selectedIndex` to switch between pages
    
    refactor(main.dart): refactor widget layout to use appropriate layout for different screen widths
    feat(main.dart): add support for BottomNavigationBar for mobile screens with width less than 450
    feat(main.dart): add support for NavigationRail for wider screens with width greater than or equal to 600
    feat(main.dart): add GeneratorPage class for word generator page
    feat(main.dart): determine favorite icon based on whether current word is a favorite
    
    feat(main.dart): add HistoryListView widget to GeneratorPage
    fix(main.dart): add spacing between HistoryListView and BigCard widget
    
    refactor(main.dart): fix variable types and naming conventions
    feat(main.dart): add BigCard class for displaying current word
    feat(main.dart): add FavoritesPage class for displaying favorites
    feat(main.dart): add HistoryListView class for displaying history

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // Construye la interfaz de usuario de la aplicación
   @override
   Widget build(BuildContext context) {
+    // Utiliza el patrón de diseño Provider para el manejo del estado
     return ChangeNotifierProvider(
+      // Crea una instancia de MyAppState para manejar el estado de la aplicación
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Namer App',
@@ -19,43 +22,70 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
+        // Establece MyHomePage como la página principal de la aplicación
         home: MyHomePage(),
       ),
     );
   }
 }
 
+// Clase para manejar el estado de la aplicación
 class MyAppState extends ChangeNotifier {
+  // La palabra actual que se muestra al usuario
   var current = WordPair.random();
+  // Lista de todas las palabras que se han mostrado al usuario
+  var history = <WordPair>[];
 
+  // Clave para acceder al estado de la lista animada de historial
+  GlobalKey? historyListKey;
+
+  // Genera una nueva palabra y la añade al historial
   void getNext() {
+    history.insert(0, current);
+    var animatedList = historyListKey?.currentState as AnimatedListState?;
+    animatedList?.insertItem(0);
     current = WordPair.random();
     notifyListeners();
   }
 
+  // Lista de palabras que el usuario ha marcado como favoritas
   var favorites = <WordPair>[];
 
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
+  // Marca o desmarca una palabra como favorita
+  void toggleFavorite([WordPair? pair]) {
+    pair = pair ?? current;
+    if (favorites.contains(pair)) {
+      favorites.remove(pair);
     } else {
-      favorites.add(current);
+      favorites.add(pair);
     }
+    notifyListeners();
+  }
+
+  // Elimina una palabra de la lista de favoritos
+  void removeFavorite(WordPair pair) {
+    favorites.remove(pair);
     notifyListeners();
   }
 }
 
+// Clase para la página principal de la aplicación
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// Clase para el estado de la página principal de la aplicación
 class _MyHomePageState extends State<MyHomePage> {
-
+  // Índice de la página actual que se muestra al usuario
   var selectedIndex = 0;
 
+  // Construye la interfaz de usuario de la página principal
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
+    // Selecciona la página actual en función del índice seleccionado
     Widget page;
     switch (selectedIndex) {
       case 0:
@@ -67,52 +97,91 @@ class _MyHomePageState extends State<MyHomePage> {
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
+
+    // Contenedor para la página actual, con su color de fondo y una sutil animación de cambio
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceVariant,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
+    );
+
+    // Devuelve un Scaffold que contiene la interfaz de usuario de la página principal
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          // Utiliza un diseño más adecuado para móviles con BottomNavigationBar en pantallas estrechas
+          if (constraints.maxWidth < 450) {
+            return Column(
+              children: [
+                Expanded(child: mainArea),
+                SafeArea(
+                  child: BottomNavigationBar(
+                    items: [
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: 'Home',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.favorite),
+                        label: 'Favorites',
+                      ),
+                    ],
+                    currentIndex: selectedIndex,
+                    onTap: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
+                )
+              ],
+            );
+          } else {
+            // Utiliza NavigationRail en pantallas más anchas
+            return Row(
+              children: [
+                SafeArea(
+                  child: NavigationRail(
+                    extended: constraints.maxWidth >= 600,
+                    destinations: [
+                      NavigationRailDestination(
+                        icon: Icon(Icons.home),
+                        label: Text('Home'),
+                      ),
+                      NavigationRailDestination(
+                        icon: Icon(Icons.favorite),
+                        label: Text('Favorites'),
+                      ),
+                    ],
+                    selectedIndex: selectedIndex,
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        selectedIndex = value;
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
-          ),
-        );
-      }
+                Expanded(child: mainArea),
+              ],
+            );
+          }
+        },
+      ),
     );
   }
 }
 
+// Clase para la página del generador de palabras
 class GeneratorPage extends StatelessWidget {
+  // Construye la interfaz de usuario de la página del generador
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    // Determina el icono de favorito en función de si la palabra actual es una favorita
     IconData icon;
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
@@ -120,10 +189,16 @@ class GeneratorPage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
+    // Devuelve la interfaz de usuario de la página del generador
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Expanded(
+            flex: 3,
+            child: HistoryListView(),
+          ),
+          SizedBox(height: 10),
           BigCard(pair: pair),
           SizedBox(height: 10),
           Row(
@@ -145,64 +220,173 @@ class GeneratorPage extends StatelessWidget {
               ),
             ],
           ),
+          Spacer(flex: 2),
         ],
       ),
     );
   }
 }
 
+// Clase para la tarjeta grande que muestra la palabra actual
 class BigCard extends StatelessWidget {
   const BigCard({
-    super.key,
+    Key? key,
     required this.pair,
-  });
+  }) : super(key: key);
 
   final WordPair pair;
 
+  // Construye la interfaz de usuario de la tarjeta grande
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: Colors.black,
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
     );
 
+    // Devuelve la interfaz de usuario de la tarjeta grande
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: pair.asPascalCase,
+        child: AnimatedSize(
+         duration: Duration(milliseconds: 200),
+          // Asegura que la palabra compuesta se ajuste correctamente cuando la ventana es demasiado estrecha
+          child: MergeSemantics(
+            child: Wrap(
+              children: [
+                Text(
+                  pair.first,
+                  style: style.copyWith(fontWeight: FontWeight.w200),
+                ),
+                Text(
+                  pair.second,
+                  style: style.copyWith(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
 
+// Clase para la página de favoritos
 class FavoritesPage extends StatelessWidget {
+  // Construye la interfaz de usuario de la página de favoritos
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
 
+    // Si el usuario no tiene favoritos, muestra un mensaje
     if (appState.favorites.isEmpty) {
       return Center(
         child: Text('No favorites yet.'),
       );
     }
 
-    return ListView(
+    // Devuelve la interfaz de usuario de la página de favoritos
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(30),
           child: Text('You have '
               '${appState.favorites.length} favorites:'),
         ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
+        Expanded(
+          // Hace un mejor uso de las ventanas anchas con una cuadrícula
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 400 / 80,
+            ),
+            children: [
+              for (var pair in appState.favorites)
+                ListTile(
+                  leading: IconButton(
+                    icon: Icon(Icons.delete_outline, semanticLabel: 'Delete'),
+                    color: theme.colorScheme.primary,
+                    onPressed: () {
+                      appState.removeFavorite(pair);
+                    },
+                  ),
+                  title: Text(
+                    pair.asLowerCase,
+                    semanticsLabel: pair.asPascalCase,
+                  ),
+                ),
+            ],
           ),
+        ),
       ],
+    );
+  }
+}
+
+// Clase para la lista de historial
+class HistoryListView extends StatefulWidget {
+  const HistoryListView({Key? key}) : super(key: key);
+
+  @override
+  State<HistoryListView> createState() => _HistoryListViewState();
+}
+
+// Clase para el estado de la lista de historial
+class _HistoryListViewState extends State<HistoryListView> {
+  // Necesario para que [MyAppState] pueda decirle a [AnimatedList] que anime los nuevos elementos
+  final _key = GlobalKey();
+
+  // Se utiliza para "desvanecer" los elementos de historial en la parte superior, para sugerir la continuación
+  static const Gradient _maskingGradient = LinearGradient(
+    // Este gradiente va de totalmente transparente a totalmente negro opaco...
+    colors: [Colors.transparent, Colors.black],
+    // ... desde la parte superior (transparente) hasta la mitad (0.5) del camino hacia abajo
+    stops: [0.0, 0.5],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  // Construye la interfaz de usuario de la lista de historial
+  @override
+  Widget build(BuildContext context) {
+    final appState = context.watch<MyAppState>();
+    appState.historyListKey = _key;
+
+    // Devuelve la interfaz de usuario de la lista de historial
+    return ShaderMask(
+      shaderCallback: (bounds) => _maskingGradient.createShader(bounds),
+      // Este modo de mezcla toma la opacidad del shader (es decir, nuestro gradiente)
+      // y la aplica al destino (es decir, nuestra lista animada)
+      blendMode: BlendMode.dstIn,
+      child: AnimatedList(
+        key: _key,
+        reverse: true,
+        padding: EdgeInsets.only(top: 100),
+        initialItemCount: appState.history.length,
+        itemBuilder: (context, index, animation) {
+          final pair = appState.history[index];
+          return SizeTransition(
+            sizeFactor: animation,
+            child: Center(
+              child: TextButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite(pair);
+                },
+                icon: appState.favorites.contains(pair)
+                    ? Icon(Icons.favorite, size: 12)
+                    : SizedBox(),
+                label: Text(
+                  pair.asLowerCase,
+                  semanticsLabel: pair.asPascalCase,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
